88d9d33a369ce862ad339583409d1ab4159e1c8b
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-28 11:05:07 -0600

    chore: Initial files and setup gql typings denerating

---
+++ src/generate-typings.ts
@@ -0,0 +1,12 @@
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { join } from 'path';
+
+/* Script to generate typings for graphql schema files when changed */
+
+// Script:
+
+const typeDefinitionsFactory = new GraphQLDefinitionsFactory();
+typeDefinitionsFactory.generate({
+  typePaths: ['./src/**/*.graphql'],
+  path: join(process.cwd(), 'src/graphql.schema.ts'), // Typings output file
+});
